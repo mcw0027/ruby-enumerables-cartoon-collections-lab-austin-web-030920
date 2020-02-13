8c9275c07c80a8931b8ac4c6219fc9cc1447c6d0
Michael Wixson <wixsonmc@gmail.com>
2020-02-13 02:53:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,6 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| call.capitalize + '!' }
 end
@@ -14,3 +12,10 @@ def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? { |call| call.length > 4 }
 end
 
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w[gouda cheddar camembert]
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
+end
